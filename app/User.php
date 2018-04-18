97103e59bd683924395ea6bb4b2f54cb742ba6cd
Richard Leibowitz <richardbbleibowitz@gmail.com>
2018-04-19 02:16:24 +0300

    Update User model

--- app/User.php
+++ app/User.php
@@ -15,7 +15,11 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+       'email',
+       'password',
+       'name',
+       'avatar_media_id',
+       'bio',
     ];
 
     /**
